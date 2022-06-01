1348a846bc4c07a0e61e98203f6b599500b4a2ec
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-01 18:39:40 -0500

    Changed pool.js to get postgres working on heroku

--- server/module/pool.js
+++ server/module/pool.js
@@ -1,9 +1,39 @@
 const pg = require('pg');
+const url = require('url')
 
-const pool = new pg.Pool({
-    host: 'localhost', 
-    port: 5432,
-    database: process.env.DATABASE_NAME || 'weekend-to-do-app'
-})
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }
+} 
+else {
+    config = {
+        host: 'localhost',
+        port: 5432,
+        database: 'weekend-to-do-app', 
+      };
+}
+
+const pool = new pg.Pool(config);
+
+
+pool.on('connect', () => {
+    console.log('Postgesql connected');
+  });
+  
+
+// const pool = new pg.Pool({
+//     host: 'localhost', 
+//     port: 5432,
+//     database: process.env.DATABASE_NAME || 'weekend-to-do-app'
+// })
+
+pool.on('error', (err) => {
+    console.log('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
 
 module.exports = pool;
